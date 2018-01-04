071cc1a63b697195a7a3c67ebd8d29f26c0dcff2
Dr. Harlan M. Brewer <Dr. Harlan M. Brewer@SPOCK>
2018-01-03 20:22:31 -0700

    Add project files.

---
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+
+        private void Handle_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Hello", "OK");
+        }
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
